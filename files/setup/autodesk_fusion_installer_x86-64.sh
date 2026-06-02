5598c14715c9b78053970d352b62296d45bab493
mosschief <mosschief@gmail.com>
2026-06-01 19:44:17 -0700

    Add missing parens to is_snap_firefox_installed function definition

--- files/setup/autodesk_fusion_installer_x86-64.sh
+++ files/setup/autodesk_fusion_installer_x86-64.sh
@@ -661,7 +661,7 @@ is_snap_firefox_installed() {
 
 check_install_firefox_deb() {
     # Function to check if Firefox is installed via Snap
-    is_snap_firefox_installed {
+    is_snap_firefox_installed() {
         snap list firefox &> /dev/null
         return $?
     }
